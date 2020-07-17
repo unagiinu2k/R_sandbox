8257a734ac2b2a6845fa52cefda6bc4eef7ae750
unagiinu2k <unagiinu_japan@yahoo.co.jp>
2020-07-17 21:08:45 +0900

    supplement missing values

--- covid_tokyo_map.R
+++ covid_tokyo_map.R
@@ -12,7 +12,10 @@ Unzip <- function(...) rbind(data.frame(), ...)
 df0 = do.call(Unzip,js0)
 df0 = df0 %>% mutate(code = rownames(df0))
 lf0 = df0 %>% gather(key = Date , value = n_patient , - code ) 
-region_code = read_tsv("region_code.tsv") %>% mutate(code  =as.integer(code))
+lf0 = lf0 %>% spread(key = Date , value = n_patient ,  fill = 0) %>% gather(key = Date , value = n_patient , - code)
+#region_code = read_tsv("region_code.tsv") %>% mutate(code  =as.integer(code))
+region_code = read_tsv("region_code.tsv") %>% mutate(code  =as.character(code))
+
 lf0 = lf0 %>% left_join(region_code)
 lf0 = lf0 %>% mutate(Date = as.Date(Date , "X%Y.%m.%d") ,
                      code = as.integer((code)) , 
@@ -20,6 +23,9 @@ lf0 = lf0 %>% mutate(Date = as.Date(Date , "X%Y.%m.%d") ,
 
 
 
+lf0 = lf0 %>% arrange(Date) %>% group_by(city_code) %>% mutate(
+  diff1 = n_patient - lag(n_patient) ,
+  diff7 = n_patient - lag(n_patient , 7)) %>% ungroup
 
 
 
@@ -32,37 +38,41 @@ sf_pref13 <- jpndistrict::jpn_pref(13, district = T) %>%
   st_simplify(dTolerance = 0.001)
  
 lf1 = sf_pref13 %>% right_join(lf0)
-
+theme_set(theme_void())
 ggplot(lf1 %>% filter(Date == max(Date))) + 
-  geom_sf(aes(fill = n_patient)) + 
+  geom_sf(aes(fill =diff7)) + 
   xlim(138.9, 139.9) + ylim(35.5, 36) + 
   theme(panel.border = element_blank(), 
         axis.title = element_blank(), axis.text = element_blank(), 
         axis.ticks = element_blank(), 
         #plot.background = element_rect(colour = "black"), 
         plot.caption = element_text(size = 6)) +
+  scale_fill_distiller(palette = "Spectral") +
+  guides(fill = guide_legend(title = "new patients (seven days)")) +
   guides(color = F)
 
-
-ggplot(lf1 %>% filter(Date >= max(Date) -3)) + 
-  geom_sf(aes(fill = n_patient)) + 
-  xlim(138.9, 139.9) + ylim(35.5, 36) + 
-  theme(panel.border = element_blank(), 
-        axis.title = element_blank(), axis.text = element_blank(), 
-        axis.ticks = element_blank(), 
-        #plot.background = element_rect(colour = "black"), 
-        plot.caption = element_text(size = 6)) +
-  guides(color = F) + facet_wrap(~Date)
-
-
+if (F) {
+  
+  ggplot(lf1 %>% filter(Date >= max(Date) -3)) + 
+    geom_sf(aes(fill = diff7)) + 
+    xlim(138.9, 139.9) + ylim(35.5, 36) + 
+    theme(panel.border = element_blank(), 
+          axis.title = element_blank(), axis.text = element_blank(), 
+          axis.ticks = element_blank(), 
+          #plot.background = element_rect(colour = "black"), 
+          plot.caption = element_text(size = 6)) +
+    
+    guides(color = F) + facet_wrap(~Date)
+  
+}
 library(gifski)
 library(gganimate)
 library(transformr)
 
 
 
-anim = ggplot(lf1 %>% filter(Date >= max(Date) -10)) + 
-  geom_sf(aes(fill = n_patient)) + 
+anim = ggplot(lf1 %>% filter(Date >= max(Date) -90)) + 
+  geom_sf(aes(fill = diff7)) + 
   xlim(138.9, 139.9) + ylim(35.5, 36) + 
   theme(panel.border = element_blank(), 
         axis.title = element_blank(), axis.text = element_blank(), 
@@ -70,8 +80,10 @@ anim = ggplot(lf1 %>% filter(Date >= max(Date) -10)) +
         #plot.background = element_rect(colour = "black"), 
         plot.caption = element_text(size = 6)) +
   guides(color = F) + transition_time(time = Date) + 
-  ease_aes("sine-in-out") + 
+  scale_fill_distiller(palette = "Spectral") +
+  guides(fill = guide_legend(title = "7 day new patients ")) +
+  #ease_aes("sine-in-out") + 
   labs(title = "{frame_time}")
-ga = animate(anim , fps = 5 , width = 800 , height = 800)
+ga = animate(anim , fps = 5 , width = 800 , height = 800 , end_pause = 50)
 ga
-anim_save(filename = "tmp.gif", ga)
+anim_save(filename = "covid_tokyo.gif", ga)
