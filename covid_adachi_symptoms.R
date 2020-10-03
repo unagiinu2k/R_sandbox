f0623677c87fc9e2556b87f0ff0469717d36e8e2
unagiinu2k <unagiinu_japan@yahoo.co.jp>
2020-10-03 14:43:07 +0900

    starts studying correlation between symptoms

--- covid_adachi_symptoms.R
+++ covid_adachi_symptoms.R
@@ -2,8 +2,10 @@ rm(list = ls())
 library(tidyverse)
 library(readr)
 if (T) {
+    df = read_csv("https://www.city.adachi.tokyo.jp/documents/47358/131200_city_adachi_covid19_patients1002.csv")
 #  df = read_csv("https://www.city.adachi.tokyo.jp/documents/47358/131200_city_adachi_covid19_patients0807.csv")
-  df = read_csv("https://www.city.adachi.tokyo.jp/documents/47358/131200_city_adachi_covid19_patients0812.csv")
+  #df = read_csv("https://www.city.adachi.tokyo.jp/documents/47358/131200_city_adachi_covid19_patients0812.csv")
+#  df = read_csv("https://www.city.adachi.tokyo.jp/documents/47358/131200_city_adachi_covid19_patients0814_2.csv")
   df = df %>% mutate(g = str_replace_all(備考, "(2020/|し公表)" , ""))
 } else {
   df = read_csv("adachi.csv") #with URL
@@ -13,6 +15,9 @@ if (T) {
   df = df %>% mutate(g = url_short)
 }
 df = df %>% mutate(年代 = str_replace(年代 , "３０代" , "30代") %>%
+                        str_replace( "５０代" , "50代") %>%
+                       str_replace( "２０代" , "20代") %>%
+                       str_replace("8０代", "80代") %>% 
                        str_replace("４０代"  , "40代"))
 start_date  =as.Date("2020-1-1")
 end_date  = as.Date("2020-12-31")
@@ -26,7 +31,10 @@ df1$症状 %>% str_split("、") %>% flatten %>% as.character %>%  unique
 
 df1 = df1 %>% mutate(symptom = 　str_replace_all(症状, "なし" , "無症状") %>% 
                        str_replace_all("障害|異常", "")%>%
-                     str_replace_all("発熱嗅覚", "発熱・嗅覚"))
+                     str_replace_all("発熱嗅覚", "発熱・嗅覚")%>%
+                     str_replace_all("嗅覚味覚", "嗅覚・味覚")) %>%
+  mutate(symptom = ifelse(str_count(symptom , "急性呼吸器") > 0 , "急性呼吸器症状" , symptom))
+
 
 symptoms = df1$symptom %>% str_split("、| |・|・|\\s") %>% flatten %>% as.character %>% 
 　　  str_replace_all("なし" , "無症状") %>% unique %>% setdiff("")
@@ -48,10 +56,37 @@ for (s in symptoms) {
   sf_all = bind_rows(sf_all , run_sf)
 }
 sf_all %>% head
+if (T) {
+  df1b = df1
+  for (s in symptoms) {
+      df1b = df1b %>% mutate(!!s := pmin(1 ,  str_count(symptom  , 　s)  ))
+  }
+  #https://stats.stackexchange.com/questions/408564/relationship-independence-between-2-binary-varibales
+  #https://oku.edu.mie-u.ac.jp/~okumura/stat/fishertest.html
+  cor.test(df1b$咳 , df1b$発熱)
+  count_a = df1b %>% group_by(咳, 発熱) %>% summarize(n = n())
+  count_a
+  fisher.test(matrix(count_a$n , nrow = 2))
+  cor.test(df1b$味覚 , df1b$嗅覚)
+  df1b %>% group_by(味覚, 嗅覚) %>% summarize(n = n())
+  count_b = df1b %>% group_by(味覚, 嗅覚) %>% summarize(n = n())
+  fisher.test(matrix(count_b$n , nrow = 2))
+  
+  us
+  fisher.test(matrix(df1b %>% group_by(味覚, 発熱) %>% summarize(n = n()) %>% pull(n) , nrow = 2))
+  fisher.test(matrix(df1b %>% group_by(嗅覚, 発熱) %>% summarize(n = n()) %>% pull(n) , nrow = 2))
+  fisher.test(matrix(df1b %>% group_by(頭痛, 発熱) %>% summarize(n = n()) %>% pull(n) , nrow = 2))
+  fisher.test(matrix(df1b %>% group_by(咽頭痛, 発熱) %>% summarize(n = n()) %>% pull(n) , nrow = 2))
+
+
+
+}
 sf2 = sf_all %>% spread(key = run_sym , value = n , fill = 0 )  %>%
   rename(with = "TRUE"  , without = "FALSE")
 ordered_sym = sf2 %>% group_by(symptom) %>% summarize(total = sum(with)) %>% ungroup %>% arrange(desc(total))%>% pull(symptom)
 sf2 = sf2 %>% mutate(symptom = factor(symptom , ordered_sym))
+
+sf3 =
 library(tidyverse)
 library(ggrepel)
 
@@ -59,6 +94,30 @@ windowsFonts("YuGo" = windowsFont("游ゴシック"))
 #windowsFonts("YuGo" = windowsFont("HG創英角ゴシックUB"))
 windowsFonts("POP"=  windowsFont("HGPSoeiKakupoptai"))
 theme_set(theme_minimal(base_family = "POP"))
+theme_set(theme_minimal(base_family = "YuGo"))
+
+
+ggplot(df1 %>% group_by(年代) %>% summarize(n = n()), aes(x = 年代, y = n , 
+                                                        fill = 年代, 
+                                                        color = 年代 )) +
+  geom_bar(stat = "identity") + guides(color = F, fill = F)
+
+ggplot(df1 %>% group_by(年代, is_na_biko) %>% summarize(n = n()) %>% 
+         spread(key = is_na_biko , value = n , fill = 0) %>% 
+         rename("備考なし" = "TRUE" , "備考あり" = "FALSE")
+         , aes(x = 備考なし, y = 備考あり   , label = 年代 , color = 年代) ) +
+  geom_point() + geom_text_repel() + guides(color = F, fill = F) + 
+  geom_smooth(method = "lm" , se = F)
+
+ggplot(df1 %>% group_by(年代, is_na_biko) %>% summarize(n = n()), aes(x = 年代, y = n , 
+                                                        fill = 年代, 
+                                                        color = 年代 )) +
+  geom_bar(stat = "identity") + guides(color = F, fill = F) + 
+  labs(x = NULL , y = "人数") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  facet_wrap(~is_na_biko , labeller = labeller(is_na_biko = c("TRUE" = "備考なし" , "FALSE" = "備考あり")))
+
+
 
 sf2_age  =sf2 %>% group_by(年代, symptom) %>% summarize(with = sum(with) , without = sum(without)) %>% ungroup
 ggplot(sf2_age ,  aes(y = 年代, x = with / (with + without) , 
@@ -70,6 +129,18 @@ ggplot(sf2_age ,  aes(y = 年代, x = with / (with + without) ,
   labs(y = NULL) +
   theme(axis.text.x = element_text(angle = 90)) +
   facet_wrap(~symptom) + guides(color = F, fill = F)
+
+
+
+ggplot(sf2_age%>% filter(symptom %in% symptoms[1:12]) ,  aes(y = 年代, x = with / (with + without) , 
+                  fill = 年代, 
+                  color = 年代 )) + 
+  geom_bar(stat = "identity") + 
+  scale_x_continuous(labels = scales::percent , name = "比率") + 
+  #coord_flip() +
+  labs(y = NULL) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  facet_wrap(~symptom , nrow = 2) + guides(color = F, fill = F)
 
 
 
@@ -96,44 +167,31 @@ ggplot(sf2_age_with_biko ,  aes(y = 年代, x = with / (with + without) ,
   facet_wrap(~symptom) + guides(color = F, fill = F)
 
 
+sf2_age_biko  =sf2 %>% group_by(is_na_biko , 年代, symptom) %>% summarize(with = sum(with) , without = sum(without)) %>% ungroup
+
+sf2_age_biko = sf2_age_biko  %>% mutate(ratio = with / (with + without))
+
+wf2_age_biko  = sf2_age_biko %>% select(-starts_with("with"))%>% 
+  spread(key = is_na_biko , value = ratio) %>%
+  rename("備考あり" = "FALSE"  , "備考なし" = "TRUE")
+
+
+ggplot(wf2_age_biko , aes(x = 備考なし ,y  = 備考あり , color = 年代 , label = 年代)) + 
+  geom_point()　+ geom_text_repel(size = 2) + facet_wrap(~symptom) + guides(color = F, fill = F)
+
+
+sf2_biko  =sf2 %>% group_by(is_na_biko , symptom) %>% summarize(with = sum(with) , without = sum(without)) %>% ungroup
+
+sf2_biko = sf2_biko  %>% mutate(ratio = with / (with + without))
+
+wf2_biko  = sf2_biko %>% select(-starts_with("with"))%>% 
+  spread(key = is_na_biko , value = ratio) %>%
+  rename("備考あり" = "FALSE"  , "備考なし" = "TRUE")
+
+
+ggplot(wf2_biko , aes(x = 備考なし ,y  = 備考あり , label = symptom , color = symptom)) + 
+  geom_point()　+ geom_text_repel(size = 2)  + guides(color = F, fill = F) + geom_abline(alpha = 0.5)
+
+
 
 # delete below 
-
-ggplot(sf2 ,  aes(x = symptom, y = with / (with + without) , color = symptom )) + 
-  geom_bar(stat = "identity") + 
-  coord_flip() +
-  facet_wrap(~年代) + guides(color = F)
-
-
-ggplot(sf_all ,  aes(x = 年代, y = n , color = symptom , label = symptom)) + 
-  geom_point() + geom_text_repel() + guides(color = F)
-
-
-
-ggplot(gf1 , aes(x = some   , y  = non , label = g)) + 
-  geom_point(size = 5, aes(color = g))  + 
-  geom_text_repel(size = 3 , alpha = 0.7 , family = "POP") + 
-  guides(color = F , shape = F) + 
-  geom_abline(alpha = 0.3) + 
-  scale_x_continuous(breaks = seq(from = 0 , to = 20, by = 5) , limits = c(0,20)) + 
-  scale_y_continuous(breaks = seq(from = 0 , to = 20, by = 5), limits = c(0,20)) + 
-  labs(x = "なんらかの症状があった人数" , 
-       y = "無症状だった感染者数" , 
-       title = "足立区7月備考あり事例(ほぼ集団感染?)") +
-  theme(text = element_text() , 
-  plot.title= element_text(hjust = 0.5 , size = 12) ,
-  axis.text.x = element_text(angle = 0))
-
-df1 %>% filter(備考 == "2020/07/20に報道機関へ情報提供し公表") %>%  group_by(症状) %>% 
-  summarize(n = n()) %>% ungroup　%>% arrange(desc(n))
-
-
-df1 %>% filter(!is_na_biko) %>%  group_by(症状) %>% 
-  summarize(n = n()) %>% ungroup　%>% arrange(desc(n))
-df1 %>% filter(is_na_biko) %>%  group_by(症状) %>% 
-  summarize(n = n()) %>% ungroup　%>% arrange(desc(n))
-
-
-df1 %>%  group_by(is_na_biko , 症状) %>% 
-  summarize(n = n()) %>% ungroup　%>%
-  spread(key = is_na_biko , value = n)
